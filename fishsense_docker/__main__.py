7c1df13feb047ccaf4cc3d7b37dc625f0e37c3b4
Christopher L. Crutchfield <ccrutchf@ucsd.edu>
2025-02-17 22:14:16 -0800

    feat: dockerfile generator

---
+++ fishsense_docker/__main__.py
@@ -0,0 +1,80 @@
+from fishsense_docker.dockerfile import Dockerfile
+from argparse import ArgumentParser
+
+def main():
+    parser = ArgumentParser("fishsense-docker")
+    parser.add_argument("-i", "--image", required=True, help="The image for the resulting Dockerfile.")
+    parser.add_argument("-o", "--output", required=True, help="The output file for the Dockerfile.")
+
+    args = parser.parse_args()
+
+    dockerfile = Dockerfile(args.image)
+    
+    dockerfile.shell("/bin/bash", "-c")
+
+    dockerfile.run("apt-get update && apt-get upgrade -y && \
+                    apt-get install -y sudo \
+                                        build-essential \
+                                        git \
+                                        libssl-dev \
+                                        zlib1g-dev \
+                                        libbz2-dev \
+                                        libreadline-dev \
+                                        libsqlite3-dev \
+                                        wget \
+                                        curl \
+                                        llvm \
+                                        libncurses5-dev \
+                                        libncursesw5-dev \
+                                        xz-utils \
+                                        tk-dev \
+                                        libffi-dev \
+                                        liblzma-dev \
+                                        python3-openssl \
+                                        libopencv-dev \
+                                        clang \
+                                        libclang-dev \
+                                        llvm \
+                                        cmake \
+                    && apt-get clean && rm -rf /var/lib/apt/lists/*")
+    
+    if "nvidia" in args.image:
+        dockerfile.run("apt-get update && apt-get install -y kmod \
+                            vulkan-tools \
+                            clinfo \
+                        && apt-get clean && rm -rf /var/lib/apt/lists/*")
+        
+        dockerfile.arg(NVIDIA_DRIVER_VERSION="560.35.03")
+        dockerfile.run("curl https://us.download.nvidia.com/XFree86/Linux-x86_64/560.35.03/NVIDIA-Linux-x86_64-${NVIDIA_DRIVER_VERSION}.run > /driver.run && \
+                        chmod +x /driver.run && \
+                        /driver.run --no-kernel-modules --no-questions --silent && \
+                        rm /driver.run")
+
+    dockerfile.run("echo 'ubuntu ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers")
+
+    dockerfile.user("ubuntu")
+    dockerfile.env(HOME="/home/ubuntu")
+    dockerfile.run("mkdir -p ${HOME}")
+    dockerfile.workdir("${HOME}")
+
+    dockerfile.run("curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y")
+
+    dockerfile.env(PYENV_ROOT="${HOME}/.pyenv", PATH="${PYENV_ROOT}/shims:${PYENV_ROOT}/bin:${PATH}")
+    dockerfile.run('git clone --depth=1 https://github.com/pyenv/pyenv.git .pyenv && \
+                    echo "export PYENV_ROOT=\\"\$HOME/.pyenv\\"" >> ${HOME}/.bashrc && \
+                    echo "[[ -d \$PYENV_ROOT/bin ]] && export PATH="\$PYENV_ROOT/bin:\$PATH"" >> ${HOME}/.bashrc && \
+                    echo "eval \"\$(pyenv init -)\\"" >> ${HOME}/.bashrc && \
+                    echo "\\"$(pyenv virtualenv-init -)\\"" >> ${HOME}/.bashrc')
+    
+    dockerfile.run("pyenv install 3.12 && pyenv global 3.12")
+    dockerfile.run("pip install --upgrade pip && \
+                    pip install poetry && \
+                    pip cache purge")
+    
+    dockerfile.cmd("/bin/bash")
+
+    with open(args.output, "w") as f:
+        f.write(str(dockerfile))
+
+if __name__ == "__main__":
+    main()
---
+++ fishsense_docker/dockerfile.py
@@ -0,0 +1,38 @@
+from io import StringIO
+from typing import Iterable, Dict
+
+class Dockerfile:
+    def __init__(self, image: str):
+        self.__writer = StringIO(newline="")
+        self.__write_line(f"FROM {image}")
+
+    def __write_line(self, line: str):
+        self.__writer.writelines([f"{line}\n"])
+
+    def __str__(self):
+        self.__writer.flush()
+
+        return self.__writer.getvalue()
+    
+    def arg(self, **kwargs: Dict[str, str]):
+        for key, value in kwargs.items():
+            self.__write_line(f"ARG {key}={value}")
+    
+    def cmd(self, *cmd_args: Iterable[str]):
+        self.__write_line(f"CMD [{", ".join(f'"{c}"' for c in cmd_args)}]")
+    
+    def env(self, **kwargs: Dict[str, str]):
+        for key, value in kwargs.items():
+            self.__write_line(f"ENV {key}={value}")
+    
+    def run(self, run: str):
+        self.__write_line(f"RUN {run}")
+    
+    def shell(self, *shell_args: Iterable[str]):
+        self.__write_line(f"SHELL [{", ".join(f'"{s}"' for s in shell_args)}]")
+
+    def user(self, user: str):
+        self.__write_line(f"USER {user}")
+
+    def workdir(self, workdir: str):
+        self.__write_line(f"WORKDIR {workdir}")
